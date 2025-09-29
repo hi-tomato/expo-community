a01e567baf89dd762c8d9dd53555d17205796b85
이승준 <tmdwns092727@nate.com>
2025-09-30 01:27:57 +0900

    feat: add FixedButtonCTA Components

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -2,19 +2,23 @@ import FixedButtonCTA from "@/components/FixedButtonCTA";
 import InputField from "@/components/InputField";
 import { StyleSheet, View } from "react-native";
 
-const LoginScreen = () => {
+const SignUpScreen = () => {
   return (
     <>
       <View style={styles.container}>
         <InputField label="이메일" placeholder="이메일을 입력해주세요." />
         <InputField label="비밀번호" placeholder="비밀번호를 입력해주세요." />
+        <InputField
+          label="비밀번호 확인"
+          placeholder="비밀번호를 입력해주세요."
+        />
       </View>
       <FixedButtonCTA label="회원가입하기" onPress={() => {}} />
     </>
   );
 };
 
-export default LoginScreen;
+export default SignUpScreen;
 
 const styles = StyleSheet.create({
   container: {
---
+++ components/FixedButtonCTA.tsx
@@ -0,0 +1,33 @@
+import colors from "@/constants";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import CustomButton from "./CustomButton";
+
+interface FixedButtonCTAProps {
+  label: string;
+  onPress: () => void;
+}
+const FixedButtonCTA = ({ label, onPress }: FixedButtonCTAProps) => {
+  const inset = useSafeAreaInsets();
+
+  return (
+    <View style={[styles.fixed, { paddingBottom: inset.bottom }]}>
+      <CustomButton label={label} onPress={onPress} />
+    </View>
+  );
+};
+
+export default FixedButtonCTA;
+
+const styles = StyleSheet.create({
+  fixed: {
+    position: "absolute",
+    bottom: 0,
+    width: "100%",
+    borderTopWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.GRAY_300,
+    paddingTop: 12,
+    paddingHorizontal: 16,
+  },
+});
