8f9c2a670bbdbabbd3e3cb2b3fc1110a04d2321f
이승준 <tmdwns092727@nate.com>
2025-09-30 14:19:47 +0900

    feat: created FeedItem Component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,17 +1,31 @@
-import CustomButton from "@/components/CustomButton";
-import { router } from "expo-router";
+import FeedItem from "@/components/FeedItem";
 import React from "react";
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
     <SafeAreaView>
-      <Text>Home</Text>
-      <CustomButton
-        label="버튼"
-        size="medium"
-        onPress={() => router.push("/auth")}
+      <FeedItem
+        post={{
+          id: 1,
+          userId: 1,
+          title: "더미 제목입니다.",
+          description:
+            "더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다. 더미 내용입니다.  더미 내용입니다.더미 내용입니다.  더미 내용입니다. 더미 내용입니다.",
+          createdAt: "",
+          author: {
+            id: 1,
+            nickname: "닉네임",
+            imageUri: "",
+          },
+          imageUris: [],
+          likes: [],
+          hasVote: false,
+          voteCount: 1,
+          commentCount: 1,
+          viewCount: 1,
+        }}
       />
     </SafeAreaView>
   );
